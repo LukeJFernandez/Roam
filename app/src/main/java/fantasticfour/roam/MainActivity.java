61b6da1cfab1a3e9b30dd2751c4bbf7928bb84cd
Luke Fernandez <lukejohnfernandez@gmail.com>
2015-10-10 23:18:21 -0400

    working well, integrated with pebble. trying to fix xml names

--- app/src/main/java/fantasticfour/roam/MainActivity.java
+++ app/src/main/java/fantasticfour/roam/MainActivity.java
@@ -1,5 +1,7 @@
 package fantasticfour.roam;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        final Handler handler2 = new Handler();
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent2 = new Intent(MainActivity.this, NavigationActivity.class);
+                MainActivity.this.startActivity(intent2);
+            }
+        }, 5000);
     }
 }
 
