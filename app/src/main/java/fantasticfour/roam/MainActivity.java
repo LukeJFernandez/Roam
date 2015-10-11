719dd546cb4f8622a5e9a7e66a04af04e5f3aea9
Luke Fernandez <lukejohnfernandez@gmail.com>
2015-10-10 20:57:39 -0400

    Centered Action Bar

--- app/src/main/java/fantasticfour/roam/MainActivity.java
+++ app/src/main/java/fantasticfour/roam/MainActivity.java
@@ -1,5 +1,6 @@
 package fantasticfour.roam;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -7,46 +8,14 @@ import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewFlipper TruitonFlipper;
-    private float initialX;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TruitonFlipper = (ViewFlipper) findViewById(R.id.flipper);
-        TruitonFlipper.setInAnimation(this, android.R.anim.fade_in);
-        TruitonFlipper.setOutAnimation(this, android.R.anim.fade_out);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent touchevent) {
-        switch (touchevent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                initialX = touchevent.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                float finalX = touchevent.getX();
-                if (initialX > finalX) {
-                    if (TruitonFlipper.getDisplayedChild() == 1)
-                        break;
-
- /*TruitonFlipper.setInAnimation(this, R.anim.in_right);
- TruitonFlipper.setOutAnimation(this, R.anim.out_left);*/
-
-                    TruitonFlipper.showNext();
-                } else {
-                    if (TruitonFlipper.getDisplayedChild() == 0)
-                        break;
-
- /*TruitonFlipper.setInAnimation(this, R.anim.in_left);
- TruitonFlipper.setOutAnimation(this, R.anim.out_right);*/
-
-                    TruitonFlipper.showPrevious();
-                }
-                break;
-        }
-        return false;
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
     }
 }
 
