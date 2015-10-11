8611851b5e32b644633585be04fa3cd374465702
Dayton Ellwanger <daytonellwanger@gmail.com>
2015-10-11 10:24:02 -0400

    GPS Stuff

--- app/src/main/java/fantasticfour/roam/NavigationActivity.java
+++ app/src/main/java/fantasticfour/roam/NavigationActivity.java
@@ -3,6 +3,9 @@ package fantasticfour.roam;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
@@ -12,6 +15,10 @@ import java.util.UUID;
 public class NavigationActivity extends AppCompatActivity {
 
     UUID pebbleAppId = UUID.fromString("43181d1a-de09-40a7-8307-8bd1b34adde3");
+    float finishDistance = 50; //In meters
+    double destLat = 35.918519;
+    double destLong = -79.038193;
+    boolean arrived = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +30,40 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         pebbleSetup();
+        initNavigation();
+    }
+
+    protected void initNavigation() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+
+            public void onLocationChanged(Location location) {
+                if(!arrived) {
+                    computeDirection(location);
+                }
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            public void onProviderEnabled(String provider) {}
+            public void onProviderDisabled(String provider) {}
+        };
+
         try {
-            Thread.sleep(5000);
-        } catch (Exception ex) {}
-        showFinishScreen();
-        /*int angle = 45;
-        while(true) {
-            setAngle(angle);
-            angle += 45;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception ex) {ex.printStackTrace();}
-        }*/
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 50, 0, locationListener);
+        } catch (SecurityException ex) {}
+    }
+
+    void computeDirection(Location loc) {
+        Location destination = new Location(loc);
+        destination.setLatitude(destLat);
+        destination.setLongitude(destLong);
+        if(loc.distanceTo(destination) <= finishDistance) {
+            showFinishScreen();
+            arrived = true;
+        } else {
+            float bearingTo = loc.bearingTo(destination) - loc.getBearing();
+            setAngle((int) bearingTo);
+        }
     }
 
     void pebbleSetup() {
@@ -43,26 +72,32 @@ public class NavigationActivity extends AppCompatActivity {
         PebbleKit.startAppOnPebble(this, pebbleAppId);
         PebbleKit.registerReceivedAckHandler(getApplicationContext(), new PebbleKit.PebbleAckReceiver(pebbleAppId) {
             @Override
-            public void receiveAck(Context context, int transactionId) {
-            }
+            public void receiveAck(Context context, int transactionId) {}
         });
         PebbleKit.registerReceivedNackHandler(getApplicationContext(), new PebbleKit.PebbleNackReceiver(pebbleAppId) {
             @Override
-            public void receiveNack(Context context, int transactionId) {
-            }
+            public void receiveNack(Context context, int transactionId) {}
         });
+    }
+
+    void sendToPebble(PebbleDictionary data) {
+        PebbleKit.sendDataToPebble(this, pebbleAppId, data);
     }
 
     void setAngle(int angle) {
         PebbleDictionary angleData = new PebbleDictionary();
         angleData.addInt32(1, angle);
-        PebbleKit.sendDataToPebble(this, pebbleAppId, angleData);
+        if(!arrived) {
+            sendToPebble(angleData);
+        }
     }
 
     void showFinishScreen() {
-        PebbleDictionary finishData = new PebbleDictionary();
-        finishData.addInt32(2, 0);
-        PebbleKit.sendDataToPebble(this, pebbleAppId, finishData);
+        if(!arrived) {
+            PebbleDictionary finishData = new PebbleDictionary();
+            finishData.addInt32(2, 0);
+            sendToPebble(finishData);
+        }
     }
 
 }
