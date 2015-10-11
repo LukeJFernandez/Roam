aaa57d91f2a28a857d84b9d024187ccf7e25bbfc
Luke Fernandez <lukejohnfernandez@gmail.com>
2015-10-10 21:57:40 -0400

    Splash screen working

--- app/src/main/java/fantasticfour/roam/SplashActivity.java
+++ app/src/main/java/fantasticfour/roam/SplashActivity.java
@@ -1,6 +1,7 @@
 package fantasticfour.roam;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,7 +11,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        SplashActivity.this.startActivity(intent);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+            }
+        }, 5000);
+
     }
 }
